f6d369e55ad46d22e4a9bb44343115b674b97c16
lucaSartore <lucasar.com8@gmail.com>
2025-08-16 11:27:45 +0200

    Add charting capability

--- src/pages/workcenter/index.tsx
+++ src/pages/workcenter/index.tsx
@@ -1,7 +1,56 @@
-export function WorkcenterPage(){
-    return (
-        <>
-          <h1>Workcenter page</h1>
-        </>
-    )
+import Highcharts from "highcharts/highcharts-gantt";
+
+import "highcharts/modules/exporting";
+import { useEffect } from "react";
+
+export function WorkcenterPage() {
+	useEffect(() => {
+		Highcharts.ganttChart("main-chart", {
+			title: {
+				text: "Simple Gantt Chart",
+			},
+
+			xAxis: [
+				{
+					min: "2014-11-17",
+					max: "2014-11-30",
+				},
+			],
+
+			series: [
+				{
+					name: "Project 1",
+          type: "gantt",
+					data: [
+						{
+							name: "Start prototype",
+							start: "2014-11-18",
+							end: "2014-11-25",
+						},
+						{
+							name: "Develop",
+							start: "2014-11-20",
+							end: "2014-11-25",
+						},
+						{
+							name: "Run acceptance tests",
+							start: "2014-11-23",
+							end: "2014-11-26",
+						},
+						{
+							name: "Test prototype",
+							start: "2014-11-27",
+							end: "2014-11-29",
+						},
+					],
+				},
+			],
+		});
+	}, []);
+
+	return (
+		<>
+			<div id="main-chart"></div>
+		</>
+	);
 }
