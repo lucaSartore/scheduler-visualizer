c9cef58946d739bc5900a170c4303d79cb8642ba
lucaSartore <lucasar.com8@gmail.com>
2025-08-16 16:30:40 +0200

    Add operator scheduling

--- src/pages/operator/index.tsx
+++ src/pages/operator/index.tsx
@@ -1,11 +1,18 @@
 import Highcharts from "highcharts/highcharts-gantt";
 import "highcharts/modules/exporting";
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import "./index.css";
+import { ScheduleStateContext } from "../../components/outer_page/outer_page";
+import { ChartFactory } from "../../util/chart_factory/chart_factory";
 
 export function OperatorPage() {
+
+  const [ schedule, _ ] = useContext(ScheduleStateContext);
+  var cf = new ChartFactory(schedule, "Operator")
+  var chart = cf.run();
+
 	useEffect(() => {
-		Highcharts.ganttChart("main-chart", {});
+		Highcharts.ganttChart("main-chart", chart);
 	}, []);
 
 	return (
--- src/util/chart_factory/chart_factory.tsx
+++ src/util/chart_factory/chart_factory.tsx
@@ -1,113 +1,128 @@
 import Highcharts from "highcharts/highcharts-gantt";
 import { Schedule, WorkPhase } from "../../types/schedule";
-import distinctColors from 'distinct-colors'
+import distinctColors from "distinct-colors";
 
+type ChartKind = "Operator" | "Workcenter";
 
-type ChartKind = "Operator" | "Workcenter"
+class ChartFactory {
+	schedule: Schedule;
+	output: Highcharts.Options;
+	kind: ChartKind;
+	orderColorAssociation: Map<string, string>;
 
-class ChartFactory{
+	constructor(schedule: Schedule, kind: ChartKind) {
+		this.schedule = schedule;
+		this.output = {};
+		this.kind = kind;
+		this.orderColorAssociation = new Map();
+	}
 
-  schedule: Schedule;
-  output: Highcharts.Options;
-  kind: ChartKind;
-  orderColorAssociation: Map<string, string>;
+	_getYAxisCategories(): Array<string> {
+		if (this.kind == "Operator") {
+			return this.schedule.operators.map((x) => x.opId);
+		} else {
+			return this.schedule.workCenters.map((x) => x.wcId);
+		}
+	}
 
-  constructor(schedule: Schedule, kind: ChartKind){
-    this.schedule = schedule;
-    this.output = {};
-    this.kind = kind;
-    this.orderColorAssociation = new Map();
-  }
+	_getYAxisIndex(category: string): number {
+		var axis = this.output.yAxis!;
+		if (Array.isArray(axis)) {
+			throw Error("Expected only one axis");
+		}
+		return axis.categories!.indexOf(category);
+	}
 
-  _getYAxisCategories(): Array<string>{
-    if (this.kind == "Operator"){
-      return this.schedule.operators.map(x => x.opId);
-    }else{
-      return this.schedule.workCenters.map(x => x.wcId);
-    }
-  }
+	addDefaultSeries() {
+		var colors = distinctColors({
+			count: this.schedule.orders.length,
+		});
 
-  _getYAxisIndex(category: string): number {
-    var axis = this.output.yAxis!;
-    if (Array.isArray(axis)){
-      throw Error("Expected only one axis")
-    }
-    return axis.categories!.indexOf(category)
-  }
+		this.schedule.orders.forEach((order, i) => {
+			this.output.series!.push({
+				name: order.woId,
+				type: "gantt",
+				color: colors[i].hex(),
+				data: [],
+			});
+		});
 
-  addDefaultSeries(){
-    var colors = distinctColors({
-      count: this.schedule.orders.length
-    })
+		return this;
+	}
 
-    this.schedule.orders.forEach((order, i) => {
-      this.output.series!.push({
-        name: order.woId,
-        type: "gantt",
-        color: colors[i].hex(),
-        data: []
-      })
-    })
-  
-    return this;
-  }
-
-  addDefaultStructure(){
-    const categories = this._getYAxisCategories();
-    this.output = {
-      title: {
-          text: `Schedule output ("${this.kind}" view)`
+	addDefaultStructure() {
+		const categories = this._getYAxisCategories();
+		this.output = {
+			title: {
+				text: `Schedule output ("${this.kind}" view)`,
+			},
+			navigator: {
+				enabled: true,
+        // @ts-ignore
+				liveRedraw: true
+			},
+      scrollbar: {
+        enabled: true
       },
-      yAxis: {
-        categories: categories
+      rangeSelector: {
+        enabled: true,
+        selected: 0
       },
-      series: []
-    };
-    return this;
+      xAxis: {
+        minRange: 7 * 24 * 3600 * 1000 // min zoom is one week
+      },
+			yAxis: {
+				categories: categories,
+			},
+			series: [],
+		};
+		return this;
+	}
+
+	_getAssignedCategories(phase: WorkPhase): Array<string> {
+		if (this.kind == "Operator") {
+			return phase.opIds;
+		} else {
+			return [phase.wcId];
+		}
+	}
+
+  _dateToString(date: Date): string {
+    return date
+      .toISOString()
+      .replace(".000Z", "")
+      .replace("T", " ")
   }
 
-  _getAssignedCategories(phase: WorkPhase): Array<string> {
-    if (this.kind == "Operator"){
-      return phase.opIds;
-    }else{
-      return [phase.wcId];
-    }
-  }
+	addOrders() {
+		this.schedule.orders.forEach((order) => {
+			order.phases.forEach((phase) => {
+				this._getAssignedCategories(phase).forEach((category) => {
+					const series = this.output.series!.find((x) => x.name == order.woId)!;
 
+					// this assertion is necessary to make ts compiler happy
+					if (series.type != "gantt") {
+						throw Error("Got wrong series type");
+					}
 
-  addOrders(){
-    this.schedule.orders.forEach((order) => {
+					series.data!.push({
+						start: this._dateToString(phase.phScheduledStart),
+						end: this._dateToString(phase.phScheduledEnd),
+						name: `${order.woId}-${phase.phId}`,
+						color: series.color,
+						y: this._getYAxisIndex(category),
+					});
+				});
+			});
+		});
 
-      order.phases.forEach((phase) => {
-        this._getAssignedCategories(phase).forEach(category => {
-          const series = this.output.series!.find(x => x.name == order.woId)!;
+		return this;
+	}
 
-          // this assertion is necessary to make ts compiler happy
-          if(series.type != "gantt"){
-            throw Error("Got wrong series type")
-          }
-
-          series.data!.push({
-            start: phase.phScheduledStart.toString(),
-            end: phase.phScheduledEnd.toString(),
-            name: `${order.woId}-${phase.phId}`,
-            color: series.color,
-            y: this._getYAxisIndex(category)
-          })
-        });
-      });
-    });
-
-    return this;
-  }
-
-  run(){
-    this
-      .addDefaultStructure()
-      .addDefaultSeries()
-      .addOrders()
-    return this.output;
-  }
+	run() {
+		this.addDefaultStructure().addDefaultSeries().addOrders();
+		return this.output;
+	}
 }
 
-export {ChartFactory}
+export { ChartFactory };
