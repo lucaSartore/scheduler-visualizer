b398074ec01b78588985eea347942caeabf2c344
lucaSartore <lucasar.com8@gmail.com>
2025-08-16 10:06:35 +0200

    Add set schedule page template

--- src/pages/upload/index.tsx
+++ src/pages/upload/index.tsx
@@ -1,15 +1,40 @@
-import { TopBar } from "../../components/top_bar/top_bar"
+import {
+	schedule,
+	setSchedule,
+} from "../../components/schedule_state/schedule_state";
+import { TopBar } from "../../components/top_bar/top_bar";
+import "index.css";
+
+import Icon from '@mdi/react';
+import { mdiUpload } from '@mdi/js';
 
 // this is a function that take as inp
-import { validateSchedule } from "../../types/schedule"
 
-export function UploadPage(){
-    return (
-        <>
-          <TopBar/>
+export function UploadPage() {
+  
+	var scheduleString = JSON.stringify(schedule);
+
+  var uploadButtonCallback = () => {
+    // here you should upload a json file, and then call
+    // setSchedule.
+    // the function validate the schema using zoo and if 
+    // fails then you should print an error
+  }
+	return (
+		<>
+			<div className="upload-page-div">
+				<TopBar />
 
 
-          <h1>Upload page</h1>
-        </>
-    )
+				<div className="button-div" onClick={uploadButtonCallback} >
+					<Icon path={mdiUpload} className="button-icon" />
+          <h1 className="button-text">Upload Schedule</h1>
+				</div>
+      
+        <pre> // i would like for this to be a "pretty" visualization of the schedule json
+          {scheduleString}
+        </pre>
+			</div>
+		</>
+	);
 }
