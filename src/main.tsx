d098f9df1195b63d25df1d3de94ebb24ba3c7de5
Luca Sartore <lucasartore02@gmail.com>
2025-08-18 16:18:22 +0200

    test

--- src/main.tsx
+++ src/main.tsx
@@ -8,26 +8,25 @@ import { OuterPage } from "./components/outer_page/outer_page";
 import "./main.css"
 
 
-
-// just a trick to make single page app work in github-pages
-// credit: https://github.com/rafgraph/spa-github-pages
-(function (l) {
-	if (l.search[1] === "/") {
-		var decoded = l.search
-			.slice(1)
-			.split("&")
-			.map(function (s) {
-				return s.replace(/~and~/g, "&");
-			})
-			.join("?");
-		window.history.replaceState(
-			null,
-			//@ts-ignore
-			null,
-			l.pathname.slice(0, -1) + decoded + l.hash,
-		);
-	}
-})(window.location);
+// // just a trick to make single page app work in github-pages
+// // credit: https://github.com/rafgraph/spa-github-pages
+// (function (l) {
+// 	if (l.search[1] === "/") {
+// 		var decoded = l.search
+// 			.slice(1)
+// 			.split("&")
+// 			.map(function (s) {
+// 				return s.replace(/~and~/g, "&");
+// 			})
+// 			.join("?");
+// 		window.history.replaceState(
+// 			null,
+// 			//@ts-ignore
+// 			null,
+// 			l.pathname.slice(0, -1) + decoded + l.hash,
+// 		);
+// 	}
+// })(window.location);
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
